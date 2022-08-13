b5dd6ab1b68debed407e48af3dbd5f49d81eef7b
DonThePhan <donthephan@gmail.com>
2022-08-13 08:27:46 -0400

    add alternative for none linked titles & center align title for narrow screen

--- src/components/2. projects/Card.jsx
+++ src/components/2. projects/Card.jsx
@@ -10,13 +10,17 @@ function Card(props) {
   return (
     <div className='flex flex-col rounded-xl m-10 lg:w-3/8'>
       <div>
-        <div className='flex flex-row justify-between text-3xl md:text-4xl lg:text-5xl py-2'>
-          <a href='' onClick={() => openInNewTab(appLink)}>
-            {title}
-          </a>
+        <div className='flex flex-row text-3xl md:text-4xl lg:text-5xl py-2 justify-center lg:justify-start'>
           <a href='' onClick={() => openInNewTab(gitHubLink)}>
-            <i className='fa-brands fa-github'></i>
+            <i className='fa-brands fa-github pr-4'></i>
           </a>
+          {appLink ? (
+            <a href='' onClick={() => openInNewTab(appLink)}>
+              {title}
+            </a>
+          ) : (
+            title
+          )}
         </div>
         <img className='rounded-xl drop-shadow-2xl' src={imgURL} alt='' />
       </div>
