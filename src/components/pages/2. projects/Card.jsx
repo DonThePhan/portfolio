93bdf9ef46a0674dd64748b564f7635324d212e4
DonThePhan <donthephan@gmail.com>
2022-08-18 16:54:27 -0400

    remove "test" text from App.js & replaced <a/> with <button/> in Card.jsx

--- src/components/pages/2. projects/Card.jsx
+++ src/components/pages/2. projects/Card.jsx
@@ -10,19 +10,23 @@ function Card(props) {
   return (
     <div className='flex flex-col justify-around w-full aspect-square shadow-lg xs:aspect-auto xs:rounded-xl xs:border xs:my-6 xs:w-4/5 sm:w-2/5 2xl:my-8'>
       <div className='flex flex-row text-3xl py-2 w-full pl-4'>
-        <a href='' onClick={() => openInNewTab(gitHubLink)}>
+        <button onClick={() => openInNewTab(gitHubLink)}>
           <i className='fa-brands fa-github pr-4'></i>
-        </a>
+        </button>
+
         {title}
       </div>
       {appLink ? (
-        <a
+        <button
           className='lg:rounded-xl w-full'
-          href=''
           onClick={() => openInNewTab(appLink)}
         >
-          <img className='xs:rounded-b-xl lg:rounded-xl w-full' src={imgURL} alt='' />
-        </a>
+          <img
+            className='xs:rounded-b-xl lg:rounded-xl w-full'
+            src={imgURL}
+            alt=''
+          />
+        </button>
       ) : (
         <img className='lg:rounded-xl w-full' src={imgURL} alt='' />
       )}
