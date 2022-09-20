a703c85f670780b9190eeaecec87aebeeb3336f7
DonThePhan <donthephan@gmail.com>
2022-09-20 19:06:14 -0400

    verson 2 - clean slate

---
+++ src/version1/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+import Nav from "./components/nav/Nav";
+import AboutMe from "./components/pages/1. aboutMe/AboutMe";
+import Projects from "./components/pages/2. projects/Projects";
+import Contact from "./components/pages/3. contact/Contact";
+import NotFound from "./components/pages/NotFound";
+
+function App() {
+  return (
+    <div className='flex flex-col items-center w-full'>
+      <div className='w-full md:w-4/5 lg:w-2/3 xl:w-1/2'>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Navigate to='/aboutme' />} />
+          <Route path='/aboutme' element={<AboutMe />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/version1/components/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div class='hero min-h-screen bg-base-200'>
+      <div class='hero-content text-center'>
+        <div class='max-w-md'>
+          <h1 className='text-7xl font-bold'>OOPS!</h1>
+          <h1 className='text-5xl font-bold'>404</h1>
+          <p className='py-6'>Page not found</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
