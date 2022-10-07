c8a9f77e9eded5422766141bc964744bad9df6cc
donthephan <donthephan@gmail.com>
2022-10-07 02:04:19 -0400

    Contact form now working - updated Button Component from div to button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Button = ({ children, onClick, type, className }) => {
   return (
-    <div
+    <button
       className={`border border-text text-text uppercase text-sm font-bold tracking-wide p-3 rounded-lg focus:outline-none focus:shadow-outline text-center cursor-pointer hover:bg-text hover:text-bg-base-2 duration-150 hover:ease-in ${className}`}
       onClick={onClick}
       type={type}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
